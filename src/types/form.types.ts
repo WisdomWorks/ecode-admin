a2df4f21d6d849d872ef8eb1b9d43f0af616a5e1
Van Dao <daovo739@gmail.com>
2024-04-11 17:07:09 +0700

    fix label

--- src/types/form.types.ts
+++ src/types/form.types.ts
@@ -14,13 +14,13 @@ export const createOptions: IFormCardRadioOption[] = [
     label: 'Manually',
     value: CreationOption.Manually,
     description:
-      'Import an Excel file using the provided template to efficiently create multiple user accounts',
+      'Create new item by fill the form. The password will be automatically generated and attached to the notification email to students ',
   },
   {
     label: 'Import',
     value: CreationOption.Import,
     description:
-      'Create new user by fill the form. The password will be automatically generated and attached to the notification email to students ',
+      'Import an Excel file using the provided template to efficiently create multiple items',
   },
 ]
 
